3a1937cd5587dd26a3012c0279e3b6610ff59814
Ashish Singh <singhashish4000@gmail.com>
2018-12-16 21:43:45 +0530

    Completed base function

---
+++ db_structure.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE flaskapp;
+
+Use flaskapp;
+
+CREATE TABLE users(id INT(11) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), email VARCHAR(100), username VARCHAR(30),location VARCHAR(30), password VARCHAR(100), register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+
+
